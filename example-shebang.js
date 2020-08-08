e1222415375bc164709c281602219d5d727c2822
Hugo Josefson <hugo@josefson.org>
2020-08-08 18:21:15 +0000

    Simplify example-shebang.js.

--- example-shebang.js
+++ example-shebang.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 /* 2>/dev/null
-s="$(cd "$(dirname "$0")" && pwd)/$(basename "$0")";export PATH="$(curl -s https://raw.githubusercontent.com/hugojosefson/find-node-or-install/master/find-node-or-install | bash):$PATH";exec node "$s" "$@"
+export PATH="$(curl -s https://raw.githubusercontent.com/hugojosefson/find-node-or-install/master/find-node-or-install | bash):$PATH"
+exec node "$0" "$@"
 */
 
 console.log('Hello, world!')
